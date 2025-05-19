604ea8a6dd8bc2097bcbdf132ed6094f7ba1108f
Zyssalone <137930429+Zyssalone@users.noreply.github.com>
2025-05-19 01:03:55 +0100

    Fix image hydration error

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {images: {
-    domains: ['localhost'],
-  },};
+const nextConfig = {
+  images: {
+    domains: ['localhost:5000'], // Include the port number
+    // Alternatively, you can use remotePatterns for more control:
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '5000',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
+};
 
-// export default nextConfig;
+module.exports = nextConfig;

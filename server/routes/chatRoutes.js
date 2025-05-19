1ea863ea489af7bba82b503cc84c8d4a2571f2ab
Adya <114347520+AdyaGMD@users.noreply.github.com>
2025-05-19 01:05:43 +0100

    bnlabal

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -10,7 +10,7 @@ router.post('/send', verifyToken, async (req, res) => {
     if (!user) return res.status(404).json({ message: 'User not found' });
 
     const NewChat = new Chat({
-      senderId: user,
+      senderId: user.userId,
       recipientId: req.body.recipientId,
       content: req.body.content,
       timestamp: new Date(),
@@ -22,6 +22,20 @@ router.post('/send', verifyToken, async (req, res) => {
   } catch (err) {
     res.status(500).json({ message: 'Server error' });
   }
+});
+
+router.get('/gethistory', verifyToken, async (req, res) => {
+    try {
+        const user = await User.findOne({ userId: req.user.userId }).select('-password');
+        const chats = await Chat.findOne({ senderId: user.userId}).sort('timestamp');
+        console.log(req.params.recipientId);
+    
+        if (!chats) return res.status(404).json({ message: 'No chat history found' });
+    
+        res.status(200).json(chats);
+    } catch (err) {
+        res.status(500).json({ message: 'Server error' });
+    }
 });
 
 module.exports = router;
